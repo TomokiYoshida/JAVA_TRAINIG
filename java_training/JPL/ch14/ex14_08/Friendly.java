74975466944d2b1abf8d5ee1701879f255dabc44
tom <tom@10.61.11.79>
2012-08-08 13:52:51 +0900

    修正した。

--- java_training/JPL/ch14/ex14_08/Friendly.java
+++ java_training/JPL/ch14/ex14_08/Friendly.java
@@ -7,16 +7,23 @@ package ch14.ex14_08;
 public class Friendly {
 	private Friendly partner;
 	private String name;
+	private Object lock1 = new Object();
+	private Object lock2= new Object();
+
 	public Friendly(String name){
 		this.name = name;
 	}
-	public synchronized void hug(){
-		System.out.println(Thread.currentThread().getName() + " in " + name + ".hug() trying to invoke " + partner.name + ".hugBack()");
-		partner.hugBack();
+	public void hug(){
+		synchronized (lock1) {
+			System.out.println(Thread.currentThread().getName() + " in " + name + ".hug() trying to invoke " + partner.name + ".hugBack()");
+			partner.hugBack();
+		}
 	}
-	private synchronized void hugBack(){
+	private void hugBack(){
+			synchronized (lock2) {
 			System.out.println(Thread.currentThread().getName() +
 					" in " + name + ".hugBack()");
+			}
 	}
 	public void becomeFriend(Friendly partner){
 		this.partner = partner;
